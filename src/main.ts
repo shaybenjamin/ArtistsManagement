9d1ad11a9ec9ef1fa76f8a4e20fde5060ff5f277
shaybenjamin <shay.benjamin1983@gmail.com>
2022-05-29 15:03:32 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+//fdsfds
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
